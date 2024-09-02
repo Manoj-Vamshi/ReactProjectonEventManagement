0d0b6f3fb9b59c1d8195328d9b6e9b56f7694656
Reva1402 <148901721+Reva1402@users.noreply.github.com>
2024-09-02 13:08:01 -0400

    Update App.js

--- App.js
+++ App.js
@@ -5,19 +5,82 @@ import WelcomePage from './WelcomePage';
 import SignupPage from './SignupPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginPage from './LoginPage';
+import EventOraganiserHomePage from './EventOrganiserHomePage';
+import AdminHomePage from './AdminHomePage';
+import AttendeeHomePage from './AttendeeHomePage';
+
 
 function App() {
   return (
     <Router>
+      <div>
+      <header>
+        <div class="d-flex justify-content-between align-items-center p-3 bg-dark text-white">
+            <a id="logo" href="/" class="text-white font-weight-bold h4">VRV Events</a>
+            <input id="search" type="text" class="form-control w-50" placeholder="Search" />
+        </div>
+    </header>
+      </div>
       <div >
         <Routes>
-   
           <Route path="/" element={<WelcomePage />} />
           <Route path="/signup" element={<SignupPage />} />
-          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/eventoragniserhomepage" element={<EventOraganiserHomePage />} />
+          <Route path="/attendeehomepage" element={<AttendeeHomePage />} />
+          <Route path="/adminhomepage" element={<AdminHomePage />} />
+
         </Routes>
       </div>
+      <div>
+        <footer class="bg-light text-center text-lg-start" id='footer'>
+          <div class="container p-6">
+            <div class="row">
+
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">About Us</h5>
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="Aboutus.html" class="text-dark">Who We Are</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Contact Us</h5>
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="Contactus.html" class="text-dark">Get in Touch</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Privacy Policy</h5>
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="PrivacyPolicy.html" class="text-dark">Our Policy</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Terms & Conditions</h5>
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="Terms&Conditions.html" class="text-dark">Read Terms</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="text-center p-9 bg-dark text-white">
+            <span>&copy; 2024 Event Management. All rights reserved.</span>
+          </div>
+        </footer>
+      </div>
     </Router>
+
   );
 }
 
