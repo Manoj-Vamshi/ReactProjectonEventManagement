854376895c84a6da091531da25287e6beb336e10
Manoj-Vamshi <148381807+Manoj-Vamshi@users.noreply.github.com>
2024-09-04 15:58:22 -0400

    Update SignupPage.js

--- SignupPage.js
+++ SignupPage.js
@@ -1,13 +1,10 @@
-// src/SignupPage.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { database, auth } from './firebaseConfig';
 import './Styling.css';
-import { setDoc, doc } from 'firebase/firestore';
-import { ref, set, get } from 'firebase/database';
-
-
+import { set, ref, get } from 'firebase/database';
+import googleLogo from './gimage.png';
 
 const SignupPage = () => {
     const [formData, setFormData] = useState({
@@ -30,8 +27,6 @@ const SignupPage = () => {
             [name]: value
         });
     };
-
-
 
     const handleSignup = async (e) => {
         e.preventDefault();
@@ -86,128 +81,114 @@ const SignupPage = () => {
         }
     };
 
-
     return (
-        <div>
-            <h1 className='text-center'>Sign Up</h1>
+        <div className="signup-container">
+            <div className="form-wrapper">
+                <h1 className="text-center">Sign Up</h1>
 
-            <div class="container">
-                <div class="row ">
-                    <div className='col-lg-2'></div>
-                    <div className='col-lg-8'>
-
-                        <form onSubmit={handleSignup}>
-                            <div className='form-group'>
-                                <label>First Name</label>
-                                <input
-                                    type='text'
-                                    className='form-control'
-                                    name='firstName'
-                                    value={formData.firstName}
-                                    onChange={handleInputChange}
-                                    required
-                                />
-                            </div>
-                            <div className='form-group'>
-                                <label>Last Name</label>
-                                <input
-                                    type='text'
-                                    className='form-control'
-                                    name='lastName'
-                                    value={formData.lastName}
-                                    onChange={handleInputChange}
-                                    required
-                                />
-                            </div>
-                            <div className='form-group'>
-                                <label>Email</label>
-                                <input
-                                    type='email'
-                                    className='form-control'
-                                    name='email'
-                                    value={formData.email}
-                                    onChange={handleInputChange}
-                                    required
-                                />
-                            </div>
-                            <div className='form-group'>
-                                <label>Password</label>
-                                <input
-                                    type='password'
-                                    className='form-control'
-                                    name='password'
-                                    value={formData.password}
-                                    onChange={handleInputChange}
-                                    required
-                                />
-                            </div>
-
-                            <div className="form-group">
-                                <label>Gender</label>
-                                <div className="gender-group">
-                                    <input
-                                        type="radio"
-                                        id="male"
-                                        name="gender"
-                                        value="Male"
-                                        checked={formData.gender === 'Male'}
-                                        onChange={handleInputChange}
-                                    />
-                                    <label htmlFor="male">Male</label>
-                                    <input
-                                        type="radio"
-                                        id="female"
-                                        name="gender"
-                                        value="Female"
-                                        checked={formData.gender === 'Female'}
-                                        onChange={handleInputChange}
-                                    />
-                                    <label htmlFor="female">Female</label>
-                                    <input
-                                        type="radio"
-                                        id="custom"
-                                        name="gender"
-                                        value="Custom"
-                                        checked={formData.gender === 'Custom'}
-                                        onChange={handleInputChange}
-                                    />
-                                    <label htmlFor="custom">Others</label>
-                                </div>
-                            </div>
-
-                            <div className='form-group'>
-                                <label>Role</label>
-
-                                <select
-                                    className='form-control'
-                                    name='role'
-                                    value={formData.role}
-                                    onChange={handleInputChange}
-                                    required>
-                                    <option value=''>Select</option>
-                                    <option value='eventorganizer'>Event Organizer</option>
-                                    <option value='attendee'>Attendee</option>
-                                    <option value='admin'>Admin</option>
-                                </select>
-                            </div>
-
-
-                            <button type='submit' className='btn btn-primary'>
-                                Sign Up
-                            </button>
-                            <button type='submit' className='btn btn-primary' onClick={handleGoogleSignup}>
-                                Login with Google
-                            </button>
-
-                        </form>
+                <form onSubmit={handleSignup}>
+                    <div className='form-group'>
+                        <label>First Name</label>
+                        <input
+                            type='text'
+                            className='form-control'
+                            name='firstName'
+                            value={formData.firstName}
+                            onChange={handleInputChange}
+                            required
+                        />
                     </div>
-                    <div className='col-lg-2'></div>
-                </div>
+                    <div className='form-group'>
+                        <label>Last Name</label>
+                        <input
+                            type='text'
+                            className='form-control'
+                            name='lastName'
+                            value={formData.lastName}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Email</label>
+                        <input
+                            type='email'
+                            className='form-control'
+                            name='email'
+                            value={formData.email}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Password</label>
+                        <input
+                            type='password'
+                            className='form-control'
+                            name='password'
+                            value={formData.password}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Gender</label>
+                        <div className="gender-group">
+                            <input
+                                type="radio"
+                                id="male"
+                                name="gender"
+                                value="Male"
+                                checked={formData.gender === 'Male'}
+                                onChange={handleInputChange}
+                            />
+                            <label htmlFor="male">Male</label>
+                            <input
+                                type="radio"
+                                id="female"
+                                name="gender"
+                                value="Female"
+                                checked={formData.gender === 'Female'}
+                                onChange={handleInputChange}
+                            />
+                            <label htmlFor="female">Female</label>
+                            <input
+                                type="radio"
+                                id="custom"
+                                name="gender"
+                                value="Custom"
+                                checked={formData.gender === 'Custom'}
+                                onChange={handleInputChange}
+                            />
+                            <label htmlFor="custom">Others</label>
+                        </div>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Role</label>
+                        <select
+                            className='form-control'
+                            name='role'
+                            value={formData.role}
+                            onChange={handleInputChange}
+                            required>
+                            <option value=''>Select</option>
+                            <option value='eventorganizer'>Event Organizer</option>
+                            <option value='attendee'>Attendee</option>
+                            <option value='admin'>Admin</option>
+                        </select>
+                    </div>
+
+                    <button type='submit' id= 'signup' className='btn btn-primary'>
+                        Sign Up
+                    </button>
+                    <button type='button' className='btn btn-google' onClick={handleGoogleSignup}>
+                        <img src={googleLogo} alt="Google logo" className="google-logo" />
+                    </button>
+                </form>
             </div>
         </div>
-
-
-
     );
 };
 
