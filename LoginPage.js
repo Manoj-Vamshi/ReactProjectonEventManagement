fcd52aa816ba257a78954871a7867ebc36bbaa39
Manoj-Vamshi <148381807+Manoj-Vamshi@users.noreply.github.com>
2024-09-04 15:59:18 -0400

    Update LoginPage.js

--- LoginPage.js
+++ LoginPage.js
@@ -7,7 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Model from './Model.js';
 import { ref, get } from 'firebase/database';
 
-
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -18,28 +17,25 @@ const LoginPage = () => {
     e.preventDefault();
 
     try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
 
-       
-        const roleRef = ref(database, 'users/' + user.uid + '/role');
-        const snapshot = await get(roleRef);
-        const userRole = snapshot.val();
+      const roleRef = ref(database, 'users/' + user.uid + '/role');
+      const snapshot = await get(roleRef);
+      const userRole = snapshot.val();
 
-       
-        if (userRole === 'admin') {
-            navigate('/adminhomepage');
-        } else if (userRole === 'eventorganizer') {
-            navigate('/eventoragniserhomepage');
-        } else if (userRole === 'attendee') {
-            navigate('/attendeehomepage');
-        }
+      if (userRole === 'admin') {
+        navigate('/adminhomepage');
+      } else if (userRole === 'eventorganizer') {
+        navigate('/eventoragniserhomepage');
+      } else if (userRole === 'attendee') {
+        navigate('/attendeehomepage');
+      }
     } catch (error) {
-        console.error('Error during login:', error);
-        alert('Login failed. Please try again.');
+      console.error('Error during login:', error);
+      alert('Login failed. Please try again.');
     }
-};
-  
+  };
 
   const handlePasswordReset = async (e) => {
     e.preventDefault();
@@ -57,76 +53,57 @@ const LoginPage = () => {
   const [isModelOpen, setIsModelOpen] = useState(false);
 
   return (
-    <div >
-      <div >
+    <div className="signup-container">
+      <div className="form-wrapper">
         <h1 className="text-center">Login</h1>
-        <div class="container">
-          <div class="row ">
-            <div className='col-lg-2'></div>
-            <div className='col-lg-8'>
-              <form onSubmit={handleLogin}>
-                <div className="form-group">
-                  <label >Email:</label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="form-control"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password">Password:</label>
-                  <input
-                    type="password"
-                    id="password"
-                    className="form-control"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                </div>
-                <button className="btn btn-primary" type="submit" >Login</button>
-              </form>
-              <button className="text-center" onClick={openModel} >Forgot Password?</button>
-
-
-
-              <Model isOpen={isModelOpen} onClose={closeModel}>
-                <h2>Reset Password</h2>
-                <form onSubmit={handlePasswordReset}>
-                  <div className="input-group">
-                    <label htmlFor="resetEmail">Email</label>
-                    <input
-                      type="email"
-                      id="resetEmail"
-                      name="resetEmail"
-                      placeholder="Enter your email"
-                      value={resetEmail}
-                      onChange={(e) => setResetEmail(e.target.value)}
-                      required
-                    />
-                  </div>
-                  <button className="resetbtn" type="submit">Send Reset Email</button>
-                </form>
-              </Model>
-            </div>
-
-            <div className='col-lg-2'></div>
-
+        <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </div>
-        </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button className="btn btn-primary" type="submit">Login</button>
+        </form>
+        <button className="btn btn-link text-center" onClick={openModel}>Forgot Password?</button>
 
+        <Model isOpen={isModelOpen} onClose={closeModel}>
+          <h2 id='rname'>Reset Password</h2>
+          <form onSubmit={handlePasswordReset}>
+            <div className="form-group">
+              <label htmlFor="resetEmail">Email</label>
+              <input
+                type="email"
+                id="resetEmail"
+                name="resetEmail"
+                className="form-control"
+                placeholder="Enter your email"
+                value={resetEmail}
+                onChange={(e) => setResetEmail(e.target.value)}
+                required
+              />
+            </div>
+            <button className="btn btn-primary" type="submit">Send Reset Email</button>
+          </form>
+        </Model>
       </div>
-
     </div>
-
-
-
-
-
-
   );
 };
 
