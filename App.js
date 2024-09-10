be6cdc4b05dbfe796ce2ae5b29af0d04069e1789
varunteja436 <62462060+varunteja436@users.noreply.github.com>
2024-09-10 09:55:04 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import WelcomePage from './WelcomePage';
 import SignupPage from './SignupPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginPage from './LoginPage';
-import EventOraganiserHomePage from './EventOrganiserHomePage';
+import EventOrganizerProfilePage from './EventOrganizerProfilePage';
 import AdminHomePage from './AdminHomePage';
 import AttendeeHomePage from './AttendeeHomePage';
 import Aboutus from './Aboutus';
 import Terms from './Terms&Conditions';
 import CreateEvent from './CreateEvent';
 import ManageEvents from './ManageEvent';
+import EditEvent from './EditEvents';
+import ViewEvent from './ViewEvent';
 import BookNow from './BookNow';
 import CheckOutForm from './CheckOutForm';
+import Contactus from './Contactus';
+import EOHomepage from './EOHomepage';
+import Logo from './vrv1.jpg';
 
 
 function App() {
   return (
     <Router>
       <div>
-        <header>
-          <div class="d-flex justify-content-between align-items-center p-3 bg-dark text-white">
-            <a id="logo" href="/" class="text-white font-weight-bold h4">VRV Events</a>
-            <input id="search" type="text" class="form-control w-50" placeholder="Search" />
+      <header>
+        <div className="d-flex justify-content-between align-items-center p-2 bg-dark text-white">
+        <a id="Logo" href="/" className="text-white d-flex align-items-center">
+          <img src={Logo} alt="VRV" style={{ height: '75px', marginRight: '15px' }} />
+          <span className="h4 mb-0 ml-2">VRV Events</span>
+        </a>
+            <div className="d-flex justify-content-center w-100">
+              <input id="search" type="text" className="form-control text-center" placeholder="Search" style={{ width: '400px' }} />
+            </div>
           </div>
         </header>
       </div>
-      <div >
+      <div>
         <Routes>
           <Route path="/" element={<WelcomePage />} />
           <Route path="/signup" element={<SignupPage />} />
@@ -37,18 +47,19 @@ function App() {
           <Route path="/manage-events" element={<ManageEvents />} />
           <Route path="/edit-event/:id" element={<EditEvent />} />
           <Route path="/view-event/:id" element={<ViewEvent />} />
-          <Route path="/attendeehomepage" element={<AttendeeHomePage />} />
           <Route path="/adminhomepage" element={<AdminHomePage />} />
+          <Route path="/attendeehomepage" element={<AttendeeHomePage />} />
           <Route path="/about" element={<Aboutus />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/create-event" element={<CreateEvent />} />
+          <Route path="/Contactus" element={<Contactus />} />
           <Route path="/manageevent" element={<ManageEvents />} />
           <Route path="/book-now/:eventId" element={<BookNow />} />
           <Route path="/checkoutform" element={<CheckOutForm />} />
         </Routes>
       </div>
       <div>
-         <footer className="bg-light text-center text-lg-start">
+      <footer className="bg-light text-center text-lg-start">
           <div className="container p-6">
             <div className="row">
               <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
@@ -63,7 +74,7 @@ function App() {
                 <h5 className="text-uppercase">Contact Us</h5>
                 <ul className="list-unstyled">
                   <li>
-                    <a href="Contactus.html" className="text-dark">Get in Touch</a>
+                    <a href="Contactus" className="text-dark">Get in Touch</a>
                   </li>
                 </ul>
               </div>
@@ -91,7 +102,6 @@ function App() {
         </footer>
       </div>
     </Router>
-
   );
 }
 
